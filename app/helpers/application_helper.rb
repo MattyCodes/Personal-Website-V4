456095eb209e44bee1eaa44aa88345d2f3f3ad20
Mattycodes <matt.nick.fagioli@gmail.com>
2018-09-29 16:07:03 -0400

    Lots of style stuff, setting up initial conventions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def controller?(name)
+    params[:controller].downcase == name.downcase
+  end
+
+  def action?(name)
+    params[:action].downcase == name.downcase
+  end
+
+  def home_page?
+    controller?('misc') && action?('home')
+  end
+
+  def show_background_tiles?
+    home_page?
+  end
 end
