a5953cb00c8442c11630f2f4ff791a73ec330aed
Mattycodes <matt.nick.fagioli@gmail.com>
2018-10-04 18:42:08 -0400

    Basic layout of about page with dynamic opening/closing tiles.

--- app/javascript/components/InformationalTilesList.js
+++ app/javascript/components/InformationalTilesList.js
@@ -6,31 +6,68 @@ class InformationalTilesList extends React.Component {
     super(props, context);
 
     this.state = {
-      listData: this.props.listData
+      listData: this.props.listData,
+      openedTileIndex: null
+    };
+  };
+
+  openTile(index) {
+    if ( index != this.state.openedTileIndex ) {
+      this.setState({ openedTileIndex: index });
+    } else {
+      this.setState({ openedTileIndex: null });
     };
   };
 
   render() {
-    let counter = 0;
-    let tiles   = [];
-    let row     = [];
-    let col     = null;
+    let counter   = 0;
+    let bottom    = null;
+    let bottomOne = null;
+    let bottomTwo = null;
+    let active    = null;
+    let divClass  = '';
+    let self      = this;
+    let tiles     = [];
+    let row       = [];
+    let col       = null;
 
     this.props.listData.map(function(data, index) {
-      col = (
+      active     = ( self.state.openedTileIndex == index );
+      divClass   = ( active ? 'active' : '' );
+
+      col        = (
         <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6 bottom-margin-md text-center" key={ `tile-list-item-${index}` }>
-          <img src={ data.primaryImage } className="tile-primary-image" />
-          <div className="tile-lower-section"></div>
+          <img
+            src={ data.primaryImage }
+            className={ `tile-primary-image ${divClass}` }
+            onClick={ () => self.openTile(index) }
+          />
+        </div>
+      );
+
+      bottom     = (
+        <div className="col-sm-12 col-md-12 col-lg-12">
+          <div className={ `tile-bottom-section ${divClass}` }>
+
+          </div>
         </div>
       );
 
       row.push(col);
       counter += 1;
 
+      if ( counter == 1 ) {
+        bottomOne = bottom;
+      } else {
+        bottomTwo = bottom;
+      };
+
       if ( counter >= 2 ) {
         tiles.push((
           <div className="row" key={ `tile-list-row-${index}` }>
             { row }
+            { bottomOne }
+            { bottomTwo }
           </div>
         ));
 
