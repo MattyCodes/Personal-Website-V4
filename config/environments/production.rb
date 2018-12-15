4f5b77523cbd2b3d64f1c2b2672cf1cd945732d9
Matty Fagioli <matt.nick.fagioli@gmail.com>
2018-12-15 09:46:54 -0500

    Changes authenitcation method to :login for sending mail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
     :domain               => 'gmail.com',
     :user_name            => ENV['FROM_ADDRESS'],
     :password             => ENV['FROM_ADDRESS_PASSWORD'],
-    :authentication       => :plain,
+    :authentication       => :login,
     :enable_starttls_auto => true
   }
 
